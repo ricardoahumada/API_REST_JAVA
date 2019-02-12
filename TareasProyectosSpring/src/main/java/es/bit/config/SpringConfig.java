b78acf0097333bf44bf6eeb3160dc601553ba247
rahumada <ricardo@enmotionvalue.com>
2019-02-12 07:33:17 +0100

    content negotiation

--- TareasProyectosSpring/src/main/java/es/bit/config/SpringConfig.java
+++ TareasProyectosSpring/src/main/java/es/bit/config/SpringConfig.java
@@ -2,11 +2,22 @@ package es.bit.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "es.bit.rest")
-public class SpringConfig {
+public class SpringConfig implements WebMvcConfigurer{
 
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	    configurer.favorPathExtension(true).
+	    favorParameter(false).
+	    ignoreAcceptHeader(false).
+	    defaultContentType(MediaType.APPLICATION_JSON); 
+	}
+	
 }
