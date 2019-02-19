8e0f70392aa7dc8bda60479981ff8bddcdb5b976
rahumada <ricardo@enmotionvalue.com>
2019-02-19 07:35:02 +0100

    generating xml

--- TareasProyectosJersey/src/main/java/es/bit/rest/JerseyService.java
+++ TareasProyectosJersey/src/main/java/es/bit/rest/JerseyService.java
@@ -1,14 +1,47 @@
 package es.bit.rest;
  
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import es.bit.models.Track;
+
   
-@Path("/message")
+@Path("/test")
 public class JerseyService
 {
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     public String getMsg()
     {
          return "Hello World !! - Jersey 2";
     }
+    
+    @GET
+	@Path("/track")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Track getTrackInJSON() {
+
+		Track track = new Track();
+		track.setTitle("Enter Sandman");
+		track.setSinger("Metallica");
+
+		return track;
+	}
+
+	@POST
+	@Path("/tracks")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Track track) {
+
+		String result = "Track saved : " + track;
+		Track track2 = new Track();
+		return Response.status(201).entity(track2).build();
+
+	}
+
 }
